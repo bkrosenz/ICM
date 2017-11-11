24a407bcd4fe8f5dca93f5a48832196e109174ea
bkrosenz <benji.rosenzweig@gmail.com>
2017-11-11 17:58:43 -0500

    update c code

--- CPUCode/SobelGaussianSolutionCpuCode.c
+++ CPUCode/SobelGaussianSolutionCpuCode.c
@@ -16,35 +16,51 @@
 
 int main(void)
 {
+    
+/* 
+TODO: change project file names from sobelgaussiansolution to ICM
+
+	int nColors = 8; 
+
+	// TODO: add gaussian noise to source image
+
+TODO: currently each cycle is a pass through the whole image.  
+To improve performance, could use state machine to store the true image
+for each pixel, provided image size is small enough.  Or memory map to CPU?
+
+TODO: currently we compute the priors before running the ICM kernel.
+Obviously, it would be more efficient to add another kernel to compute these,
+or store it in an accumulator.
+
+TODO: currently assumes input image consists of c unordered colors.  
+For a grayscale image, we can turn this into a c-thresholding algorithm, with c evenly spaced values in [0,255].
+
+// TODO initialize prior means
+    int x,y;
+    int32_t val;
+    for (x=0; x<width; ++x){
+        for (y=0; y<width++y){
+            val=inImage[x][y];
+        }
+    }
+
+*/
+
+
 	printf("Loading image.\n");
 	int32_t *inImage;
-	int width = 0, height = 0;
-	
-	// TODO: add gaussian noise to source image
-	
+	int width = 256, height = 256;
 	loadImage("lena.ppm", &inImage, &width, &height, 1);
 
 	int dataSize = width * height * sizeof(int);
 	// Allocate a buffer for the output image
 	int32_t *outImage = malloc(dataSize);
 
-/* TODO: currently each cycle is a pass through the whole image.  
-To improve performance, could use state machine to store the true image
-for each pixel, provided image size is small enough.  Or memory map to CPU?
-
-TODO: currently we compute the priors before running the ICM kernel.
-Obviously, it would be more efficient to add another kernel to compute these.
-
-TODO: currently assumes input image consists of c unordered colors.  
-For a grayscale image, we can turn this into a c-thresholding algorithm, with c evenly spaced values in [0,255].
-
-*/
-
-    printf("Running Kernel.\n");
+	printf("Running Kernel.\n");
 	SobelGaussianSolution(width * height, inImage, outImage);
 
 	printf("Saving image.\n");
-	writeImage("lena_sobelgaussian.ppm", outImage, width, height, 1);
+	writeImage("lena_icm.ppm", outImage, width, height, 1);
 
 	return 0;
 }
